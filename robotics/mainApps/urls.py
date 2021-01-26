3c3f1d050f32015226e14b62eb79d342fca7e1c3
PressStart9 <63587513+PressStart9@users.noreply.github.com>
2021-01-26 17:59:16 +0300

    Delete urls.py

--- robotics/mainApps/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from django.views.generic import ListView, DetailView
-
-urlpatterns = [
-    path(r'', views.view_news, name='view_news'),
-    path(r'contacts/', views.view_contacts, name='view_contacts'),
-]
