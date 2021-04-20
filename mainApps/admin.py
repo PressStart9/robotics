46618adc529d09d873ca75d8657259f3d6889dee
PressStart9 <63587513+PressStart9@users.noreply.github.com>
2021-04-20 17:56:05 +0300

    Update admin.py

--- mainApps/admin.py
+++ mainApps/admin.py
@@ -3,9 +3,9 @@ from mainApps.models import Posts, Attachments
 
 @admin.register(Posts)
 class AdminPosts(admin.ModelAdmin):
-    list_display = ['posting_date', 'post_id', 'text']
+    list_display = ['posting_date', 'post_id', 'text', 'main_history', 'author_name']
 
 
 @admin.register(Attachments)
 class AdminAttachmets(admin.ModelAdmin):
-    list_display = ['type', 'url']
+    list_display = ['type', 'url', 'connect_post', 'preview']
